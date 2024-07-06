07bc4f8f2b9c6639017210c46fedd825ccabf8a6
GtzAof <1244033953@qq.com>
2024-07-06 16:04:40 +0800

    fix：更新场景

---
+++ JavaScripts/NpcStac.ts
@@ -0,0 +1,23 @@
+﻿
+@Component
+export default class NpcStac extends Script {
+
+    /** 当脚本被实例后，会在第一帧更新前调用此函数 */
+    protected onStart(): void {
+
+    }
+
+    /**
+     * 周期函数 每帧执行
+     * 此函数执行需要将this.useUpdate赋值为true
+     * @param dt 当前帧与上一帧的延迟 / 秒
+     */
+    protected onUpdate(dt: number): void {
+
+    }
+
+    /** 脚本被销毁时最后一帧执行完调用此函数 */
+    protected onDestroy(): void {
+
+    }
+}
