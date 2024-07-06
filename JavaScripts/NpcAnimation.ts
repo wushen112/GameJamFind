a96ba88e889c9cc0733b5aea44fdf20351af30b8
GtzAof <1244033953@qq.com>
2024-07-06 17:46:15 +0800

    fix：更新场景

--- JavaScripts/NpcAnimation.ts
+++ JavaScripts/NpcAnimation.ts
@@ -1,10 +1,30 @@
 ﻿
 @Component
-export default class NpcStac extends Script {
+export default class NpcAnimation extends Script {
+    @Property({displayName:"动画guid"})
+    private   AnimaGuid : string = "";
 
+    private Npc : Character;
+    private anima: Animation;
     /** 当脚本被实例后，会在第一帧更新前调用此函数 */
     protected onStart(): void {
+        setTimeout(() => {
+            this.Npc= this.gameObject as Character;
+            this.init(this.AnimaGuid)
+        }, 3000);
+        // AssetUtil.asyncDownloadAsset(this.AnimaGuid)
+        // 
+    }
 
+
+    async init(guid){
+        if(AssetUtil.assetLoaded(guid)){
+            await AssetUtil.asyncDownloadAsset(guid)
+        }
+        this.anima=this.Npc.loadAnimation(this.AnimaGuid)
+        this.anima.loop=0;
+        this.anima.play()
+        console.log("anima:"+this.gameObject+":"+this.anima)
     }
 
     /**
